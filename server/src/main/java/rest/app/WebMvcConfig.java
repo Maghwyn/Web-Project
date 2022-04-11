bae2d371b414257a2e2c2b8d00ec0916a9a58cfe
kevin <pinerokevin@icloud.com>
2022-04-12 00:20:54 +0200

    Suspicious merge ;)

--- server/src/main/java/rest/app/WebMvcConfig.java
+++ server/src/main/java/rest/app/WebMvcConfig.java
@@ -1,5 +1,6 @@
  package rest.app;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NotNull CorsRegistry registry) {
         long MAX_AGE_SECS = 3600;
         registry.addMapping("/**")
         .allowedOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:5001")
